b3087d564d400d1851c6f0063dcfaa4cebab7fd6
tristandev2 <tristandonado2701@gmail.com>
2024-09-28 15:59:04 -0500

    Bases de datos en clever cloud

--- app/config/db_config.py
+++ app/config/db_config.py
@@ -2,8 +2,8 @@ import mysql.connector
 
 def get_db_connection():
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="",
-        database="mascotas"
+        host="b4doekmqoqseuuhpvht5-mysql.services.clever-cloud.com",
+        user="uue9rqthiufqqio1",
+        password="x82mk1uF5pwGK3vCsd5z",
+        database="b4doekmqoqseuuhpvht5"
     )
